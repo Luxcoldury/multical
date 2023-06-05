61a4f23ee32fa184ed6cc933cc727d7a4a19730e
xubw <xubw@shanghaitech.edu.cn>
2023-06-05 14:49:21 +0800

    add web to more bags multical

--- build_image.sh
+++ build_image.sh
@@ -1,1 +1,3 @@
-docker build -t multical_more_bags -f Dockerfile_ros1_18_04 .
+docker build --no-cache -t multical_more_bags -f Dockerfile_ros1_18_04 .
+docker build --no-cache -t multical_more_bags_web_base -f Dockerfile_ros1_18_04_web_base .
+docker build --no-cache -t multical_more_bags_with_web -f Dockerfile_ros1_18_04_with_web .
--- run_image.sh
+++ run_image.sh
@@ -1,6 +1,7 @@
 # get dataset from https://drive.google.com/drive/folders/19Gm0JonlTn7TjFn0OXlriiU5I0j3US8k
-FOLDER=$HOME/multical_calibration_example_data
+FOLDER=/data/data/mapping_robot_data_collection
+# FOLDER=$HOME/multical_calibration_example_data
 xhost +local:root
-docker run -it -e "DISPLAY" -e "QT_X11_NO_MITSHM=1" \
+docker run -it -p 5000:5000 -e "DISPLAY" -e "QT_X11_NO_MITSHM=1" -e "MULTICAL_DATASET=/data" -e "TZ=Asia/Shanghai"\
     -v "/tmp/.X11-unix:/tmp/.X11-unix:rw" \
-    -v "$FOLDER:/data" multical_more_bags
+    -v "$FOLDER:/data" multical_more_bags_with_web
