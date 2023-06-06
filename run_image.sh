4882063e170400d9752284b2a7c2e1a7cf1b57dc
xubw <xubw@shanghaitech.edu.cn>
2023-06-06 21:23:00 +0800

    bugfix

--- run_image.sh
+++ run_image.sh
@@ -1,6 +1,6 @@
 # get dataset from https://drive.google.com/drive/folders/19Gm0JonlTn7TjFn0OXlriiU5I0j3US8k
 FOLDER=/data/data/mapping_robot_data_collection
 xhost +local:root
-docker run -it -e "DISPLAY" -e "QT_X11_NO_MITSHM=1" -e MULTICAL_DATASET=$FOLDER\
+docker run -it -p 5000:5000 -e "DISPLAY" -e "QT_X11_NO_MITSHM=1" -e "MULTICAL_DATASET=/data" -e "TZ=Asia/Shanghai"\
     -v "/tmp/.X11-unix:/tmp/.X11-unix:rw" \
     -v "$FOLDER:/data" multical_ignore_boards_with_web
